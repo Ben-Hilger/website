0f58468144f3332e29d7ea2e6a2620bae4411d62
Ben Hilger <bhilger78@gmail.com>
2024-01-01 00:56:48 -0500

    add update message on home page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex h-100 flex-col align-items-center justify-content-between p-6">
-      <p>Welcome!</p>
+      <p>Welcome! This website is currently under construction, but feel free to take a look at my portfolio and blog using the navigation bar above</p>
     </main>
   )
 }
