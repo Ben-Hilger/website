c45c2e66cc33ddec96c868b406663738dab39719
Ben Hilger <bhilger78@gmail.com>
2024-01-01 10:34:37 -0500

    Update front page with call to action

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <main className="flex h-100 flex-col align-items-center justify-content-between p-6">
-      <p>Welcome! This website is currently under construction, but feel free to take a look at my portfolio and blog using the navigation bar above</p>
+      <h4 className="welcome-home">Welcome!</h4>
+      <p className="welcome-call-to-action">Thanks for taking the time to visit my website! To begin, take a look at my <a href="/portfolio">portfolio</a> or <a href="https://dev.to/benhilger" target="_blank">blog</a></p>
     </main>
   )
 }
