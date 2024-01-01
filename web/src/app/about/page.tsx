f2294c8f26060816c9024383d8232fcc8be40453
Ben Hilger <bhilger78@gmail.com>
2024-01-01 14:06:08 -0500

    language fix

--- web/src/app/about/page.tsx
+++ web/src/app/about/page.tsx
@@ -5,7 +5,7 @@ import CardInfo from "../lib/card";
 export default function About() {
     const cards: CardInfo[] = [
         new CardInfo(1, "Google Cloud", "Google Cloud's Compute is the backbone of ben-hilger.com. This provides a virtual machine that runs in the cloud", null),
-        new CardInfo(2, "DNS", "ben-hilger.com is registered using SquareSpace domains. Along with that, ben-hilger.com uses SquareSpace namespace server and servcies to add more DNS routes", null),
+        new CardInfo(2, "DNS", "ben-hilger.com is registered using SquareSpace domains. Along with that, ben-hilger.com uses SquareSpace namespace servers and services to add more DNS routes", null),
         new CardInfo(3, "NGINX", "NGINX listens on port 80 and 443 and provides a proxy to route requests to the NEXT.js server, exposing the website to the public", null),
         new CardInfo(4, "NEXT.js", "This website is built using NEXT.js, which uses React server-side components and typescript", null),
         new CardInfo(5, "SSL Certificate Management", "SSL Certificates are used to ensure secure HTTPS communication between your browser and the server. Cert-bot is installed and utilizwed to issue valid certificates for ben-hilger.com", null)
