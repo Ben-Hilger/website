2b8e4a98e734a2be4ff0fc1afc45a0b79337c3f3
Ben Hilger <bhilger78@gmail.com>
2024-01-05 23:26:11 -0500

    Update image to hide duplicates on smaller screens

--- web/src/app/components/image.gallery.tsx
+++ web/src/app/components/image.gallery.tsx
@@ -10,34 +10,50 @@ interface ImageGalleryProps {
 
 export default function ImageGallery(props: ImageGalleryProps) {
     
-    function isScrolling() {
-        // return window.innerWidth < props.images.length * 150 * 2;
-        return props.images.length >= 10;
-    }
+    const isScrolling = props.images.length >= 10;
 
     function getImageGalleryClass() {
-        if (isScrolling()) {
+        if (isScrolling) {
             return "image-gallery image-gallery-scroll";
         }
         return "image-gallery";
     }
 
+    function getDuplicateImages() {
+        if (!isScrolling) {
+            return <></> 
+        }
+        return props.images.map((image, index) => {
+            return (
+                <Image className="scroll-image-extra ml-1 mr-1" 
+                    key={index} 
+                    src={image.src}
+                    alt={image.alt} 
+                    width={150} 
+                    height={225} />
+                
+            );
+        })
+    }
+
     function getImages() {
         let imagesToMap = props.images;
-        if (isScrolling()) {
-            imagesToMap.push(...props.images);
-        }
         return imagesToMap.map((image, index) => {
            return (
-            <Image className="scroll-image ml-1 mr-1" key={index} src={image.src} alt={image.alt} width={150} height={225} />
+            <Image className="scroll-image ml-1 mr-1" 
+                    key={index} 
+                    src={image.src}
+                    alt={image.alt} 
+                    width={150} 
+                    height={225} />
            );
         });
     }
 
     return (
         <div className={ getImageGalleryClass() }>
-            { getImages() }
-
+            { getImages() }  
+            { getDuplicateImages() }
         </div>
     );
 }
