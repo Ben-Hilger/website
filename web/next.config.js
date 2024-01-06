afe648f86323a29a055f63174d5f7018e57d829d
Ben Hilger <bhilger78@gmail.com>
2024-01-05 22:59:42 -0500

    Update to use image CDN

--- web/next.config.js
+++ web/next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
             },
             {
                 protocol: 'https',
-                hostname: 'storage.googleapis.com'
+                hostname: 'image.ben-hilger.com'
             }
         ]
     }
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -16,22 +16,17 @@ export default function Home() {
             <h1 className="welcome-home">Welcome!</h1>
             <p className="welcome-call-to-action">Thanks for taking the time to visit my website! To begin, take a look at my <a href="/portfolio">portfolio</a> or <a href="https://dev.to/benhilger" target="_blank">blog</a>. If you&apos;re interested in learning about this website, go check out the <a href="/about">about</a> page. </p>   
             <div className="flex flex-col align-items-center">
-                <h3>Books I Plan on Reading</h3>
-                <ImageGallery images={ getImages(BookState.Not_Read) }/>
-            </div>  
-            <div className="flex flex-col align-items-center">
                 <h3>Books I Am Reading</h3>
                 <ImageGallery images={ getImages(BookState.Reading) }/>
             </div> 
             <div className="flex flex-col align-items-center">
+                <h3>Books I Plan on Reading</h3>
+                <ImageGallery images={ getImages(BookState.Not_Read) }/>
+            </div>  
+            <div className="flex flex-col align-items-center">
                 <h3>Books I Have Read</h3>
                 <ImageGallery images={ getImages(BookState.Read) }/>
             </div>
-            <div className="flex flex-col align-items-center">
-                <h3>Books I Use for Reference</h3>
-                <ImageGallery images={ getImages(BookState.Reference) }/>
-            </div>
-
         </main>
     )
 }
