94af2b9c42ca7abc6953a2fdd07e45658670ecc3
Ben Hilger <bhilger78@gmail.com>
2024-01-07 12:44:19 -0500

    fix: link coloring

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
     return (
         <main className="flex h-100 flex-col align-items-center justify-content-between p-2">
             <h1 className="welcome-home">Welcome!</h1>
-            <p className="welcome-call-to-action">Thanks for taking the time to visit my website! To begin, take a look at my <a href="/portfolio">portfolio</a> or <a href="https://dev.to/benhilger" target="_blank">blog</a>. If you&apos;re interested in learning about this website, go check out the <a href="/about">about</a> page. </p>   
+            <p className="welcome-call-to-action">Thanks for taking the time to visit my website! To begin, take a look at my <a className="text-blue-600" href="/portfolio">portfolio</a> or <a className="text-blue-600" href="https://dev.to/benhilger" target="_blank">blog</a>. If you&apos;re interested in learning about this website, go check out the <a className="text-blue-600" href="/about">about</a> page. </p>   
             <div className="flex flex-col align-items-center">
                 <h3 className="text-xl mt-1 font-bold dark:text-white">Books I Am Reading</h3>
                 <ImageGallery images={ getImages(BookState.Reading) }/>
