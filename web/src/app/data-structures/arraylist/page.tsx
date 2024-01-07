281c85f6d1ad6af78994a2091c53cbc15b453fb6
Ben Hilger <bhilger78@gmail.com>
2024-01-07 11:39:16 -0500

    fix: language changes, input value

--- web/src/app/data-structures/arraylist/page.tsx
+++ web/src/app/data-structures/arraylist/page.tsx
@@ -167,7 +167,7 @@ export default function ArrayList() {
                 <>
                     <p>Remove At Index:</p>
                     <input type="number"
-                        value={getValue}
+                        value={removeAtValue}
                         onChange={evt => setRemoveAtValue(Number(evt.target.value))}
                         className="w-100 mb-1 mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
                     />
@@ -186,7 +186,7 @@ export default function ArrayList() {
             return (
                 <>
                     <p className="mb-1">{ getResultValue }</p>
-                    <p className="mb-1"><b>The calculation to get this value is</b>:</p><p> (mem loc of array) + (index * size_of_types_in_bytes)</p>
+                    <p className="mb-1"><b>The calculation to get this value is</b>:</p><p> (mem loc of array) + (index * size_of_type_in_bytes)</p>
                     <p className="mb-1"><b>Example</b>: int32 is four bytes long, so the calculation would be: (mem loc) + ({getValue} * 4) </p>
                     <button type="button"
                             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
