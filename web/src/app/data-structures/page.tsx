2fbdb08fd6ebf05885b68c63f21cc5964b62dbe1
Ben Hilger <bhilger78@gmail.com>
2024-01-06 22:09:33 -0500

    update borders

--- web/src/app/data-structures/page.tsx
+++ web/src/app/data-structures/page.tsx
@@ -18,7 +18,7 @@ export default function DataStructures() {
     return (
         <>
             <div className="flex flex-col justify-content-center align-items-center">
-                <div className="border-b-2 border-gray-500 w-100 flex flex-col align-items-center mb-1">
+                <div className="border-b-2 border-gray-300 w-100 flex flex-col align-items-center mb-1">
                     <b className="text-lg mt-1">Data Structures</b>
                     <div className="flex flex-row">
                     {
