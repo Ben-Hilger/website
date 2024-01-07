4d93b91b5bfd6f97bf3ccef67f11508de7386c26
Ben Hilger <bhilger78@gmail.com>
2024-01-07 12:09:38 -0500

    feature: update arraylist page to match stack/queue layout

--- web/src/app/data-structures/arraylist/page.tsx
+++ web/src/app/data-structures/arraylist/page.tsx
@@ -12,28 +12,29 @@ export default function ArrayList() {
 
     let [inputText, setInputText] = React.useState<number>(0);
 
-    let [getResultValue, setGetResultValue] = React.useState("");
     let [getValue, setGetValue] = React.useState<number>(0);
 
     let [removeAtValue, setRemoveAtValue] = React.useState<number>(0);
 
     let [removeValue, setRemoveValue] = React.useState<number>(0);
 
+    let [resultValue, setResultValue] = React.useState("");
+
     function addToArrayList() {
         const newLength = incrementLength();
         const arrayValidation = validateArraySize(newLength);
         let array = arrayValidation.array;
-        array[newLength -1 ] = inputText;
+        array[newLength - 1] = inputText;
         updateStates(array, arrayValidation.capacity, newLength);
     }
 
     function prependToArrayList() {
         const newLength = incrementLength();
         const arrayValidation = validateArraySize(newLength);
-        let array = arrayValidation.array; 
+        let array = arrayValidation.array;
         for (let i = arrayList.length - 1; i >= 0; i--) {
-            if (array[i] != undefined)  {
-                array[i + 1] = array[i];    
+            if (array[i] != undefined) {
+                array[i + 1] = array[i];
             }
         }
         array[0] = inputText;
@@ -66,13 +67,13 @@ export default function ArrayList() {
     }
 
 
-    function updateStates(array: Array<number>, 
-                            capacity: number, 
-                            length: number) {
+    function updateStates(array: Array<number>,
+        capacity: number,
+        length: number) {
         setCapacity(capacity);
         setLength(length);
         setArrayList(array);
-        setInputText(0); 
+        setInputText(0);
     }
 
     function incrementLength() {
@@ -84,8 +85,8 @@ export default function ArrayList() {
     }
 
     function validateArraySize(length: number): { capacity: number, array: Array<number> } {
-       let newArray = arrayList;
-       let newCapacity = capacity;
+        let newArray = arrayList;
+        let newCapacity = capacity;
         if (length > capacity) {
             newCapacity *= 2;
             let expandedArray = new Array(newCapacity);
@@ -100,18 +101,18 @@ export default function ArrayList() {
     function getIndexValue() {
         const getIndex: number = Number(getValue);
         if (isNaN(getIndex)) {
-            setGetResultValue("Please enter a valid value");
+            setResultValue("Please enter a valid value");
             return;
         } else if (getIndex >= length || getIndex < 0) {
-            setGetResultValue("You entered a number outside the bounds of the current array");
+            setResultValue("You entered a number outside the bounds of the current array");
             return;
-        } 
-        const indexValue = arrayList[getIndex]; 
-        setGetResultValue(`The value is: ${indexValue}`);
+        }
+        const indexValue = arrayList[getIndex];
+        setResultValue(`The value is: ${indexValue}`);
     }
 
     function resetGetIndexValue() {
-        setGetResultValue("");
+        setResultValue("");
         setGetValue(0);
     }
 
@@ -119,10 +120,8 @@ export default function ArrayList() {
         return arrayList.map((item, index) => {
             return (
                 <div className="mr-1 mt-1 border-01 p-2 border-rounded" key={index}>
-                    <div className="flex flex-col">
-                        <p className="m-0 p-0">{item}</p>
-                    </div>
-                 </div>
+                    <p className="m-0 p-0">{item}</p>
+                </div>
             );
         })
     }
@@ -133,84 +132,54 @@ export default function ArrayList() {
         let emptySlots = [];
 
         for (let i = 0; i < numberOfEmptySlots; i++) {
-            emptySlots.push(<div key={i} className="mr-1 mt-1 border-01 p-2 border-rounded">
-                                <div className="flex flex-col">
-                                    undefined @ index {i + offset}
-                                </div>
-                            </div>)
+            emptySlots.push(<div key={i} className="mr-1 mt-1 border-01 p-2 border-rounded bg-gray-300">
+            </div>)
         }
-        
+
         return emptySlots;
     }
 
     function removeValueRegion() {
         return (
-                <>
-                    <p>Remove Value:</p>
-                    <input type="number"
-                        value={removeValue}
-                        onChange={evt => setRemoveValue(Number(evt.target.value))}
-                        className="w-100 mb-1 mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-                    />
+            <>
+                <p>Remove Value:</p>
+                <input type="number"
+                    value={removeValue}
+                    onChange={evt => setRemoveValue(Number(evt.target.value))}
+                    className="w-100 mb-1 mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                />
+                <div className="flex flex-row">
                     <button type="button"
-                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                            onClick={_ => removeValueInArray()}>
+                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                        onClick={_ => removeValueInArray()}>
                         Remove that value
                     </button>
-                </>
-
-        );
-    }
-
-    function removeAtInputRegion() {
-        return (
-                <>
-                    <p>Remove At Index:</p>
-                    <input type="number"
-                        value={removeAtValue}
-                        onChange={evt => setRemoveAtValue(Number(evt.target.value))}
-                        className="w-100 mb-1 mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-                    />
                     <button type="button"
-                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                            onClick={_ => removeAtIndex()}>
+                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                        onClick={_ => removeAtIndex()}>
                         Remove that index
                     </button>
-                </>
+                </div>
+            </>
 
         );
     }
 
     function getAtInputRegion() {
-        if (getResultValue)  {
-            return (
-                <>
-                    <p className="mb-1">{ getResultValue }</p>
-                    <p className="mb-1"><b>The calculation to get this value is</b>:</p><p> (mem loc of array) + (index * size_of_type_in_bytes)</p>
-                    <p className="mb-1"><b>Example</b>: int32 is four bytes long, so the calculation would be: (mem loc) + ({getValue} * 4) </p>
-                    <button type="button"
-                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                            onClick={_ => resetGetIndexValue()}>
-                        Reset
-                    </button>
-                </>
-            );    
-        }
-        
         return (
-                <>
-                    <p>Get At Index:</p>
-                    <input type="number"
-                        value={getValue}
-                        onChange={evt => setGetValue(Number(evt.target.value))}
-                        className="w-100 mb-1 mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-                    />
-                    <button type="button"
-                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                            onClick={_ => getIndexValue()}>
-                        Get me that value
-                    </button>
-                </>
+            <>
+                <p>Get At Index:</p>
+                <input type="number"
+                    value={getValue}
+                    onChange={evt => setGetValue(Number(evt.target.value))}
+                    className="w-100 mb-1 mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                />
+                <button type="button"
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                    onClick={_ => getIndexValue()}>
+                    Get me that value
+                </button>
+            </>
         );
     }
 
@@ -221,7 +190,7 @@ export default function ArrayList() {
                 <input type="number"
                     value={inputText}
                     onChange={evt => setInputText(Number(evt.target.value))}
-                    className="w-100 mb-1 mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                    className="w-100 mb-1 mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 />
                 <div className="flex flex-row">
                     <button type="button"
@@ -242,28 +211,27 @@ export default function ArrayList() {
                 <b className="text-2xl">ArrayList</b>
                 <p><b>Length</b>: {length}</p>
                 <p><b>Capacity</b>: {capacity}</p>
-                <p className="text-center pl-5 pr-5">An <b>ArrayList</b> abstracts the allocation and management of the underlying array. An array has fixed memory, with a defined length. So, when you <b>exceed the defined capacity of the array</b>, an ArrayList will automatically create a new array with a larger size, and move the existing array into the new array.</p> 
-                <div className="flex flex-col w-75">
-                    <div className="flex flex-row">
-                        <div className="flex flex-col align-items-center justify-content-center p-0_5 pl-1 pr-1 mt-1 mr-2 w-100">
-                            { removeValueRegion() }
-                        </div>
-                        <div className="flex flex-col align-items-center justify-content-center p-0_5 pl-1 pr-1 mt-1 mr-2 w-100">
-                            { removeAtInputRegion() }
-                        </div>
+                <p className="text-center pl-5 pr-5">An <b>ArrayList</b> abstracts the allocation and management of the underlying array. An array has fixed memory, with a defined length. So, when you <b>exceed the defined capacity of the array</b>, an ArrayList will automatically create a new array with a larger size, and move the existing array into the new array.</p>
+                <div className="flex flex-row w-75 pt-2 align-items-center justify-content-center">
+                    <div className="flex flex-col flex-1 pr-2">
+                       <p className="pb-1"><b>Remove Value</b>: This finds the first instance of the value from the beginning of the ArrayList, and removes that value. It maintains the order of the rest of the ArrayList</p>
+                       <p className="pb-1"><b>Remove At Index</b>: This removes the value at the specified index, while maintianing the order of the rest of the ArrayList</p>
+                       <p className="pb-1"><b>Get At Index</b>: Gets the value at the specified index. You wil get an error if you enter a number not within the current length of the ArrayList</p>
+                       <p className="pb-1"><b>Prepend</b>: Adds the value to the beginning of the ArrayList. To be able to do this, the ArrayList needs to move all of the existing elements one index up in the ArrayList. This frees index 0, where it can prepend the value</p>
+                       <p><b>Append</b>: Adds the value to the end of the ArrayList</p>
                     </div>
-                    <div className="flex flex-row">
-                        <div className="flex flex-col align-items-center justify-content-center p-0_5 pl-1 pr-1 mt-1 mr-2 w-100">
-                            { getAtInputRegion() }
-                        </div>
-                        <div className="flex flex-col justify-content-center align-items-center w-100">
-                            { modifyArrayListInputRegion() }
-                        </div>
+                    <div className="flex flex-col align-items-center justify-content-center flex-1">
+                            {removeValueRegion()}
+                            {getAtInputRegion()}
+                            {modifyArrayListInputRegion()}
                     </div>
                 </div>
+                <div className="p-1">
+                    <p><b>Result</b>: {resultValue} </p>
+                </div>
                 <div className="flex flex-row align-items-center w-75 flex-wrap">
-                    { getArrayListDisplay() }
-                    { getEmptySlots() }
+                    {getArrayListDisplay()}
+                    {getEmptySlots()}
                 </div>
             </div>
         </>
