33b19ff272e6eb9df2f72b3cf0e9b3c75fec94ec
Ben Hilger <bhilger78@gmail.com>
2024-01-08 22:29:12 -0500

    vue initial commit

---
+++ vue/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./index.html', './src/**/*.{vue,js,tsx,jsx}'],
+    darkMode: false,
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
