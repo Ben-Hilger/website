d20da189869aa3994bcb2c77ed8d061878c87214
Ben Hilger <bhilger78@gmail.com>
2024-01-09 21:41:15 -0500

    add basic linked list page

---
+++ web/src/app/data-structures/linkedlist/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from "react";
+import DataStructureNavigation from "../datastructure.navigation";
+
+export default function Page() {
+
+    return (
+        <>
+            <DataStructureNavigation />
+            <p className="flex justify-center">This is currently under construction, check back later!</p>
+        </>
+    );
+}
