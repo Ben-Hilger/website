e941f5610c273c9460b159bd667b957aba553b9d
Ben Hilger <bhilger78@gmail.com>
2024-01-15 09:05:42 -0500

    feature: begin remaking website in nuxt

---
+++ web-nuxt/pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="flex h-100 flex-col items-center justify-between p-2">
+        <h1 class="welcome-title welcome-animation">Welcome!</h1>
+        <p class="welcome-animation">Thanks for taking the time to visit my website! To begin, take a look at my <a class="text-blue-600" href="/portfolio">portfolio</a>. If you're interested in learning about tihs website, go check out the <a class="text-blue-600" href="/about">about</a> page.</p>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+
+<style scoped>
+
+.welcome-animation {
+    animation: fadeIn 2s;    
+}
+
+.welcome-title {
+    font-size: 64px;
+    margin: 0;
+}
+
+@keyframes fadeIn {
+    0% { opacity: 0; }
+    100% { opacity: 1; }
+}
+
+</style>
---
+++ web-nuxt/pages/portfolio.vue
@@ -0,0 +1,30 @@
+<template>
+    <main class="grid grid-cols-3 h-100 p-2 background-inherit gap-2">
+        <Card v-for="(portfolioItem, index) in portfolioItems" 
+            v-bind:key="index"
+            v-bind:title="portfolioItem.title"
+            v-bind:description="portfolioItem.description"
+            v-bind:target="portfolioItem.target"
+            v-bind:link="portfolioItem.link"
+        />
+    </main>
+</template>
+
+
+<script setup lang="ts">
+    type PortfolioInfo = {
+        title: string,
+        description: string,
+        link: string,
+        target: string
+    }
+
+    const portfolioItems: PortfolioInfo[] = [
+        {title: "Personal Website", description: "This project stores the code that runs https://www.ben-hilger.com", link: "https://github.com/Ben-Hilger/website", target: "_blank"},
+        {title: "Advent of Code", description: "This project stores the codee that I write to solve advent of code problems (https://adventofcode.com/)", link: "https://github.com/Ben-Hilger/advent-of-code", target: "_blank"},
+        {title: "NVIM Config", description: "This project stores my current configuation for neovim, a terminal-based editor I use for personal projects", link: "https://ghithub.com/Ben-Hilger/nvim-config", target: "_blank"},
+        {title: "LeetCode", description: "This project stores solutions that I've written for different LeetCode problems", link: "https://github.com/Ben-Hilger/LeetCode", target: "_blank"}
+    ];
+
+
+</script>
