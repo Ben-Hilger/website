8878fe6d2511f35a25b090840eff9029dba2b0e1
Ben Hilger <bhilger78@gmail.com>
2024-01-28 14:36:47 -0500

    feature: cleanup dockerfiles

--- web-nuxt/pages/portfolio.vue
+++ web-nuxt/pages/portfolio.vue
@@ -11,7 +11,6 @@
     </main>
 </template>
 
-
 <script setup lang="ts">
     type PortfolioInfo = {
         title: string,
@@ -26,6 +25,4 @@
         {title: "NVIM Config", description: "This project stores my current configuation for neovim, a terminal-based editor I use for personal projects", link: "https://ghithub.com/Ben-Hilger/nvim-config", target: "_blank"},
         {title: "LeetCode", description: "This project stores solutions that I've written for different LeetCode problems", link: "https://github.com/Ben-Hilger/LeetCode", target: "_blank"}
     ];
-
-
 </script>
