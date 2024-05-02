80a3f2dd07712a5ac3f6053ab534203dca534858
Ben Hilger <bhilger78@gmail.com>
2024-05-02 12:18:49 -0400

    update portfolio layout

--- web/src/app/portfolio/page.tsx
+++ web/src/app/portfolio/page.tsx
@@ -8,9 +8,10 @@ export default function Portfolio() {
         new CardInfo(1, "Personal Website", "This project stores the code that runs https://www.ben-hilger.com", "https://github.com/Ben-Hilger/website"),
         new CardInfo(2, "Data Structures", "Here are different data structures displayed in an interactive way to show how they work under the hood", "/data-structures", "_self"),
         new CardInfo(3, "National Holidays", "An open source project that can dynamically generate given holidays (and their observed dates) for a given year", "https://github.com/Ben-Hilger/php-recurring-holidays"),
-        new CardInfo(4, "Frontend Masters", "Repository where I host the materials I use in different front-end master courses while learning and reinforcing my knowledge", "https://github.com/Ben-Hilger/frontend-masters"),
-        new CardInfo(5, "LeetCode", "This project stores solutions I've written for different LeetCode problems", "https://github.com/Ben-Hilger/LeetCode"),
-        new CardInfo(6, "GitHub", "There's a bunch of more small projects I've completed and have hosted on my Github Page. Feel free to take a look!", "https://github.com/Ben-Hilger/"),
+        new CardInfo(4, "bendev404.com", "Sandbox for exploring and learning the basics of new frameworks and programming languages", "https://www.bendev404.com"),
+        new CardInfo(5, "Frontend Masters", "Repository where I host the materials I use in different front-end master courses while learning and reinforcing my knowledge", "https://github.com/Ben-Hilger/frontend-masters"),
+        new CardInfo(6, "LeetCode", "This project stores solutions I've written for different LeetCode problems", "https://github.com/Ben-Hilger/LeetCode"),
+        new CardInfo(7, "GitHub", "There's a bunch of more small projects I've completed and have hosted on my Github Page. Feel free to take a look!", "https://github.com/Ben-Hilger/"),
    ];
 
     function getPortfolioItems() {
